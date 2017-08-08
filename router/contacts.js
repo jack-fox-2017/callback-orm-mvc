e89ba1aa0f75de2a0fe9e88d2136271cc8520eff
gananggww <gananggww@gmail.com>
2017-08-08 21:14:41 +0700

    MVC ORM CALLBACK

---
+++ router/contacts.js
@@ -0,0 +1,44 @@
+//express & router
+const express = require("express")
+const router = express.Router()
+const bodyParser = require("body-parser")
+//db
+const Model = require("../model/database-create")
+let model = new Model()
+let db = model.connection
+//file model
+let Contact = require("../model/contacts-model")
+let contacts = new Contact();
+
+router.use(bodyParser.urlencoded({ extended: false }))
+router.use(bodyParser.json())
+
+
+router.get("/", (req, res)=>{
+  contacts.findAll(db, (err, rows)=> {
+    res.render("contacts", {data_contacts : rows})
+  })
+})
+router.get("/add", function(req, res) {
+  res.render("contacts-form")
+})
+router.post("/add", function(req, res) {
+  contacts.create(db, req.body)
+    res.redirect("/home/contacts")
+})
+router.get("/edit/:id",function(req, res) { //routing - logic - render // populate == value
+  contacts.findById(db, req.params.id, (err, db_Contact)=> {
+    res.render("contacts-edit", {data_contacts : db_Contact})
+  })
+})
+router.post("/edit/:id",function(req, res) {
+  contacts.update(db, req.body, req.params.id)
+    res.redirect("/home/contacts")
+})
+
+router.get("/delete/:id", function(req, res) {
+  contacts.destroy(db, req.params.id)
+    res.redirect("/home/contacts")
+})
+
+module.exports = router
