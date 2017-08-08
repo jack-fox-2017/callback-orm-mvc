b3f6187aab1646467d0f6894d8c8d752866d1675
tezaharsony <teza.harsony230394@gmail.com>
2017-08-08 21:21:19 +0700

    Done

--- routers/address.js
+++ routers/address.js
@@ -1,7 +1,7 @@
 var express = require ('express');
 var path = require ('path');
 var app = express()
-var bodyParser = require ('body-parser')
+//var bodyParser = require ('body-parser')
 // app.use(bodyParser.json())
 // app.use(bodyParser.urlencoded({extended : true}))
 // app.set('view engine', 'ejs');
